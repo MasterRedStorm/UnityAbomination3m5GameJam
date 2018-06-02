b622d46d8c533cdc37fc82d3abc8e8e944f81aa2
Felix Schulze <felix.schulze@3m5.de>
2018-06-02 15:43:03 +0200

    Add two axis motion for player

--- UnityAbomination/Assets/Scripts/PlayerController.cs
+++ UnityAbomination/Assets/Scripts/PlayerController.cs
@@ -10,76 +10,122 @@ using UnityEngine.Experimental.PlayerLoop;
 using UnityEngine.Timeline;
 using UnityEngine.UI;
 
+internal enum DirectionForwardBackward
+{
+    Backward = -1,
+    None,
+    Forward
+}
+
+internal enum DirectionUpDown
+{
+    Down = -1,
+    None,
+    Up
+}
+
 public class PlayerController : MonoBehaviour
 {
+    public int Life;
 
-	public int Life;
+    public float SpeedUpDown;
+    
+    public float SpeedForward;
 
-	public float Speed;
+    public float MaxSpeed;
 
-	public float MaxSpeed;
+    public float Acceleration;
 
-	public float Acceleration;
-
-	public float MaxOffset;
+    public float MaxOffset;
 
     public int inEnemyTrigger = 0;    
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-
     private void FixedUpdate()
-	{
-		var player = GetComponent<Rigidbody2D>();
+    {
+        var player = GetComponent<Rigidbody2D>();
+        
+        var directionLeftRight = GetKeysDirectionForwardBackward();
+        var directionUpDown = GetKeysUpDownDirection();
 
-		var moveUp = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
-		var moveDown = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+        var moveUp = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        var moveDown = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
 
-		var anyDirectionKey = moveUp != moveDown;
+        SpeedUpDown = ChangeSpeed(SpeedUpDown, (int)directionUpDown);
+        SpeedForward = ChangeSpeed(SpeedForward, (int)directionLeftRight);
 
-		var direction = !anyDirectionKey
-			? 0.0f
-			: (
-				moveUp
-					? 1.0f
-					: -1.0f
-			)
-		;
+        var movement = new Vector2(SpeedForward, SpeedUpDown);
 
-		if (Mathf.Abs(Speed) < 0.1f)
-		{
-			Speed = 0.0f;
-		}
-		else
-		{
-			if (Mathf.Approximately(direction, 0.0f))
-			{
-				direction = -0.33f * Speed / Mathf.Abs(Speed);
-			}
-		}
-		
-		Speed = Mathf.Clamp(Speed + direction * Acceleration, -MaxSpeed, MaxSpeed);
+        player.velocity = movement;
 
-		var movement = new Vector3(0.0f, Speed, 0.0f);
-		
-		player.velocity = movement;
-		
-		player.position = new Vector3(
-			player.position.x,
-			Mathf.Clamp(player.position.y, -MaxOffset, MaxOffset),
-			player.position.y
-		);
-        if (inEnemyTrigger > 0) { Debug.Log("ouch"); }
-	}
+        player.position = new Vector2(
+            Mathf.Clamp(player.position.x, -MaxOffset, MaxOffset),
+            Mathf.Clamp(player.position.y, -MaxOffset, MaxOffset)
+        );
+        if (inEnemyTrigger > 0)
+        {
+            Debug.Log("ouch");
+        }
+    }
+   
+    private static DirectionForwardBackward GetKeysDirectionForwardBackward()
+    {
+        var moveLeft = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+        var moveRight = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+        var leftRightMotionKey = moveLeft != moveRight;
+
+        return !leftRightMotionKey
+                ? DirectionForwardBackward.None
+                : (moveLeft
+                    ? DirectionForwardBackward.Backward
+                    : DirectionForwardBackward.Forward
+                )
+            ;
+    }
+
+    private static DirectionUpDown GetKeysUpDownDirection()
+    {
+        var moveUp = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        var moveDown = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+        var upDownMotionKey = moveUp != moveDown;
+        
+        return !upDownMotionKey
+            ? DirectionUpDown.None
+            : (moveUp
+                ? DirectionUpDown.Up
+                : DirectionUpDown.Down
+            );
+    }
+    
+    private float ChangeSpeed(float speed, int currentDirection)
+    {
+        float accelerationFactor = currentDirection;
+        
+        if (Mathf.Abs(speed) < 0.1f)
+        {
+            speed = 0.0f;
+        }
+        else
+        {
+            if (Mathf.Approximately(currentDirection, 0.0f))
+            {
+                accelerationFactor = -0.33f * speed / Mathf.Abs(speed);
+                speed += currentDirection * Acceleration;
+            }
+        }
+        
+        var updatedSpeed = Mathf.Clamp(speed + accelerationFactor * Acceleration, -MaxSpeed, MaxSpeed);
+
+        return updatedSpeed;
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag ("Enemy")) 
+        if (other.gameObject.CompareTag("Enemy"))
         {
             inEnemyTrigger++;
         }
     }
+
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Enemy"))
