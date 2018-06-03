e49ef165de5d9d9b18a046e2bba002e9dd67dba4
Steve Gotzel <steve.gotzel@3m5.de>
2018-06-03 18:53:58 +0200

    Boss Health

---
+++ UnityAbomination/Assets/Scripts/BossHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour {
+
+    public int bossHealth = 5;
+
+    private GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("ReduceBossHealth", 2f, 2f);
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ReduceBossHealth ()
+    {
+        bossHealth -= 1;
+        if (bossHealth <= 0)
+        {
+            gameManager.StopGame("You Won");
+        }
+    }
+}
