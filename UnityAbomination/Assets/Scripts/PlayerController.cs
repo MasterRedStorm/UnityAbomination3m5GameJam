9f6a8b0decccdeb858d5daa5744014f48954850a
Felix Schulze <felix.schulze@3m5.de>
2018-06-03 09:33:43 +0200

    Add non-linear movement script to replace player-specific code

--- UnityAbomination/Assets/Scripts/PlayerController.cs
+++ UnityAbomination/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Globalization;
-using System.Runtime.Serialization.Formatters;
-using System.Security.Permissions;
-using UnityEditor;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
-using UnityEngine.Timeline;
-using UnityEngine.UI;
 
 internal enum DirectionForwardBackward
 {
@@ -29,16 +19,6 @@ public class PlayerController : MonoBehaviour
 {
     public int Life;
 
-    public float SpeedUpDown;
-
-    public float SpeedForward;
-
-    public float MaxSpeed;
-
-    public float Acceleration;
-
-    public float DragFactor;
-
     public float MaxOffset;
 
     public int inEnemyTrigger = 0;
@@ -52,31 +32,9 @@ public class PlayerController : MonoBehaviour
         var directionX = CheckKeyDirectionX();
         var directionY = CheckKeysDirectionY();
 
-        SpeedUpDown = ChangeSpeed(SpeedUpDown, (int) directionY, FlowPassages.Count == 0);
-        SpeedForward = ChangeSpeed(SpeedForward, (int) directionX, FlowPassages.Count == 0);
+        var nonLinearMovement = GetComponent<NonLinearMovement>();
 
-        var aggregatedVector = FlowPassages.Aggregate(
-            new Vector2(),
-            (vector, gameObject) =>
-            {
-                var eulerRotation = gameObject.transform.rotation.eulerAngles.z / 360 * 2 * Mathf.PI;
-                var x = -Mathf.Sin(eulerRotation);
-                var y = Mathf.Cos(eulerRotation);
-                
-                var directionalVector = new Vector2(
-                    x * gameObject.Strength,
-                    y * gameObject.Strength
-                );
-                return vector + directionalVector;
-            }
-        );
-
-        SpeedForward += aggregatedVector.x;
-        SpeedUpDown += aggregatedVector.y;
-        
-        var movement = new Vector2(SpeedForward, SpeedUpDown);
-        
-        player.velocity = movement;
+        nonLinearMovement.Move(ref player, new Vector2((int) directionX, (int) directionY), FlowPassages);
 
         var aspect = (float) Screen.width / (float) Screen.height;
 
@@ -120,28 +78,6 @@ public class PlayerController : MonoBehaviour
                 ? DirectionUpDown.Up
                 : DirectionUpDown.Down
             );
-    }
-
-    private float ChangeSpeed(float speed, int currentDirection, bool applySlowdown)
-    {
-        float accelerationFactor = currentDirection;
-
-        if (Mathf.Abs(speed) < Acceleration / 2)
-        {
-            speed = 0.0f;
-        }
-        else
-        {
-            if (Mathf.Approximately(currentDirection, 0.0f))
-            {
-                accelerationFactor = -DragFactor * speed / Mathf.Abs(speed);
-                speed += currentDirection * Acceleration;
-            }
-        }
-
-        var updatedSpeed = Mathf.Clamp(speed + accelerationFactor * Acceleration, -MaxSpeed, MaxSpeed);
-
-        return updatedSpeed;
     }
 
     public List<FlowScript> FlowPassages = new List<FlowScript>();
